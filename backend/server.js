6ff1c2034ca09078e20e9a3e60c8b483fc57a960
hmozo <hmozo@outlook.com>
2022-09-23 12:30:37 +0200

    add routing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Server starting...')
+const express= require('express')
+const dotenv= require('dotenv').config()
+const { errorHandler }= require('./middleware/errorMiddleware')
+
+const PORT= process.env.PORT || 8000
+const app= express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res)=>{
+    res.status(201).json({message:'Welcome to the support-desk API'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+
+app.listen(PORT, ()=> console.log(`*Server started on port ${PORT}`))
