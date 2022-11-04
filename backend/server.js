671fe78bb8248a8f0b11d20399b5834023684938
hmozo <hmozo@outlook.com>
2022-11-04 09:14:38 +0100

    add build frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path= require('path')
 const express= require('express')
 const colors= require('colors')
 const dotenv= require('dotenv').config()
@@ -12,12 +13,18 @@ const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res)=>{
-    res.status(201).json({message:'Welcome to the support-desk API'})
-})
-
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res)=> res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+}else{
+    app.get('/', (req, res)=>{
+        res.status(201).json({message:'Welcome to the support-desk API'})
+    })
+}
 
 app.use(errorHandler)
 
